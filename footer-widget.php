c3b839f374a448b2d6d40aa5820f981c257d367e
Marek Kononiuk <mpk.coding@gmail.com>
2022-06-30 17:30:59 +0200

    footer, colophon

--- footer-widget.php
+++ footer-widget.php
@@ -1,23 +1,23 @@
 <?php
 
 if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) || is_active_sidebar( 'advist-footer-4' ) || is_active_sidebar( 'advist-footer-5' ) ) {?>
-<div id="footer-widget" class="row m-0 <?php if(!is_theme_preset_active()){ echo 'bg-light'; } ?>">
+<div id="footer-widget" class="row m-0">
   <div class="container">
-    <div class="row">
+    <div class="row footer mx-0">
       <?php if ( is_active_sidebar( 'footer-1' )) : ?>
-      <div class="col-12 col-md-6 col-lg"><?php dynamic_sidebar( 'footer-1' ); ?></div>
+      <div class="footer__col"><?php dynamic_sidebar( 'footer-1' ); ?></div>
       <?php endif; ?>
       <?php if ( is_active_sidebar( 'footer-2' )) : ?>
-      <div class="col-12 col-md-6 col-lg"><?php dynamic_sidebar( 'footer-2' ); ?></div>
+      <div class="footer__col"><?php dynamic_sidebar( 'footer-2' ); ?></div>
       <?php endif; ?>
       <?php if ( is_active_sidebar( 'footer-3' )) : ?>
-      <div class="col-12 col-md-6 col-lg"><?php dynamic_sidebar( 'footer-3' ); ?></div>
+      <div class="footer__col"><?php dynamic_sidebar( 'footer-3' ); ?></div>
       <?php endif; ?>
       <?php if ( is_active_sidebar( 'advist-footer-4' )) : ?>
-      <div class="col-12 col-md-6 col-lg"><?php dynamic_sidebar( 'advist-footer-4' ); ?></div>
+      <div class="footer__col"><?php dynamic_sidebar( 'advist-footer-4' ); ?></div>
       <?php endif; ?>
       <?php if ( is_active_sidebar( 'advist-footer-5' )) : ?>
-      <div class="col-12 col-md-6 col-lg"><?php dynamic_sidebar( 'advist-footer-5' ); ?></div>
+      <div class="footer__col"><?php dynamic_sidebar( 'advist-footer-5' ); ?></div>
       <?php endif; ?>
     </div>
   </div>
--- footer.php
+++ footer.php
@@ -11,23 +11,22 @@
 
 ?>
 <?php if(!is_page_template( 'blank-page.php' ) && !is_page_template( 'blank-page-with-container.php' )): ?>
-			</div><!-- .row -->
-		</div><!-- .container -->
-	</div><!-- #content -->
-    <?php get_template_part( 'footer-widget' ); ?>
-	<footer id="colophon" class="site-footer <?php echo wp_bootstrap_starter_bg_class(); ?>" role="contentinfo">
-		<div class="container pt-3 pb-3">
-            <div class="site-info">
-                &copy; <?php echo date('Y'); ?> <?php echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>'; ?>
-                <span class="sep"> | </span>
-                <a class="credits" href="https://afterimagedesigns.com/wp-bootstrap-starter/" target="_blank" title="WordPress Technical Support" alt="Bootstrap WordPress Theme"><?php echo esc_html__('Bootstrap WordPress Theme','wp-bootstrap-starter'); ?></a>
-
-            </div><!-- close .site-info -->
-		</div>
-	</footer><!-- #colophon -->
+</div><!-- .row -->
+</div><!-- .container -->
+</div><!-- #content -->
+<?php get_template_part( 'footer-widget' ); ?>
+<footer id="colophon" class="site-footer <?php echo wp_bootstrap_starter_bg_class(); ?>" role="contentinfo">
+  <div class="container">
+    <div class="site-info">
+      <span>© Copyright 2020 zadanie-rekrutacyjne-advist. All rights reserved.</span>
+    </div>
+    <!-- close .site-info -->
+  </div>
+</footer><!-- #colophon -->
 <?php endif; ?>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
